df697a2f807c6b7dc864aa91646a6a4f6636599d
Bryant <chaoers@bu.edu>
2024-02-13 00:03:06 -0500

    homework

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Profile from './components/profile'
+import Form from "@/pages/qcomps/stuckForm";
+import FeedbackForm from "@/pages/qcomps/thankYouCrash";
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+        <FeedbackForm />
     </div>
   )
 }
